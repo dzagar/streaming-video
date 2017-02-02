cee48527fabd84cc0db0ead39fbc123e09294d0a
dzagar <dzagar@uwo.ca>
2017-02-02 10:52:12 -0500

    Initial commit

---
+++ dzagar_SE3314_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dzagar_SE3314_Assignment
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
