635fb35dae3408c96381682dcb7b2cc6ac9ce8b5
dzagar <dzagar@uwo.ca>
2017-02-03 15:38:01 -0500

    ipAddr in socket binding is being rejected

--- dzagar_SE3314_Assignment/View-MainView.cs
+++ dzagar_SE3314_Assignment/View-MainView.cs
@@ -18,13 +18,17 @@ namespace dzagar_SE3314_Assignment
         {
             InitializeComponent();
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            Console.WriteLine(ipHostInfo.ToString());
+
             IPAddress ipAddr = ipHostInfo.AddressList[0];
+            Console.WriteLine(ipAddr.ToString());
+            //ipAddr is in format it doesn't like... investigate
             IPEndPoint localEP = new IPEndPoint(ipAddr, Convert.ToInt32(PortNumberTextBox.Text));
-            Socket servSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+            Console.WriteLine(localEP.ToString());
             try
             {
+                Socket servSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 servSocket.Bind(localEP);
+                Console.WriteLine("Binding werked!");
                 //servSocket.Listen(100);
 
             }
