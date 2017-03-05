a56ebbf6eb612914ebd5260eda554303546d5fd3
dzagar <dzagar@uwo.ca>
2017-03-05 17:21:16 -0500

    Initial commit for Assignment 2

---
+++ dzagar_SE3314_Assignment2/Model-RTP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dzagar_SE3314_Assignment2
+{
+    class Model_RTP
+    {
+    }
+}
---
+++ dzagar_SE3314_Assignment2/View-MainView.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace dzagar_SE3314_Assignment2
+{
+    partial class View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1118, 610);
+            this.Name = "View";
+            this.Text = "Streaming Video Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
